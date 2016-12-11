734298c64eaf168bfeee32bc57c28e41bf0a0802
Antonis Karamitros @antouank <ablackjim228@gmail.com>
2016-12-11 16:58:44 +0000

    Gather the input data in an object

--- src/script/entry.js
+++ src/script/entry.js
@@ -2,3 +2,27 @@
 'use strict';
 
 console.log('this is the Forecast app.');
+
+var printInput = function() {
+  var inputDateElement 
+    = document.querySelector('#input-date');
+  
+  var inputCompetionElement 
+    = document.querySelector('#input-competition');
+
+  var inputCompetitorAElement 
+    = document.querySelector('#input-competitor-a');
+
+  var inputCompetitorBElement 
+    = document.querySelector('#input-competitor-b');
+
+  var values = 
+    { date: inputDateElement.value
+    , competition: inputCompetionElement.value 
+    , competitorA: inputCompetitorAElement.value
+    , competitorB: inputCompetitorBElement.value
+    };
+
+  console.log(values); 
+};
+
